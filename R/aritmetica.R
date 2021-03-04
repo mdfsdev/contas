4034708b81bbdf903099b798ef0c819401d509d9
mdfsdev <mdfs.dev@gmail.com>
2021-03-04 13:09:37 -0300

    first commit

---
+++ R/aritmetica.R
@@ -0,0 +1,30 @@
+#' Subtract Function
+#'
+#' This function is good to subtract
+#'
+#' @param a A any number
+#' @param b B is another random number
+#'
+#' @examples
+#' subtrair(5,9)
+#'
+#' @export
+subtrair <- function(a, b = 2) {
+  a - b
+}
+
+#' Sum Function
+#'
+#' This function is good to sum
+#'
+#' @param a A any number
+#' @param b B is another random number
+#'
+#' @examples
+#' somar(5,9)
+#'
+# sintaxe para deixar uma funcao visivel ao usuário
+#' @export
+somar <- function(a, b) {
+  sum(a, b)
+}
